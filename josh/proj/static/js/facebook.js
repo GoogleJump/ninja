8bad802f3d86a7c0e37368809fd051f48236fbef
Joshua Rosenfeld <jlr0802@gmail.com>
2014-08-05 16:46:17 -0700

    hide facebook login button on sign-in

--- josh/proj/static/js/facebook.js
+++ josh/proj/static/js/facebook.js
@@ -78,7 +78,8 @@ function testAPI() {
 	console.log('Welcome!  Fetching your information.... ');
 	FB.api('/me', function(response) {
 	  console.log('Successful login for: ' + response.name);
-	  document.getElementById('status').innerHTML =
-		'Thanks for logging in, ' + response.name + '!';
+	  $('#connected').show();
+	  $('#facebook-connected').show();
+	  $('#facebook-connect').hide('slow');
 	});
 }
