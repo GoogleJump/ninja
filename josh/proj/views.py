5749c34b29fd0bbafc95d97f4ea57426c26b0de9
Joshua Rosenfeld <jlr0802@gmail.com>
2014-08-05 18:26:29 -0700

    update views

--- josh/proj/views.py
+++ josh/proj/views.py
@@ -24,6 +24,7 @@ from google.appengine.ext import blobstore
 from google.appengine.ext.webapp import blobstore_handlers
 from google.appengine.ext import ndb
 from google.appengine.api import images
+from google.appengine.api import mail
 
 from werkzeug import parse_options_header
 
